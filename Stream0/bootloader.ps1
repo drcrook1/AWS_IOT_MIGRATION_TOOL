2c49a6399785ae7181fe2f5a658c05b7a6ffb332
johbake <johbake@microsoft.com>
2021-04-06 23:01:11 -0700

    simulated bootloader
    watches for upgrade_device file and, when present, will start the azure client

---
+++ Stream0/bootloader.ps1
@@ -0,0 +1,13 @@
+﻿while ($TRUE)
+{
+    If (Test-Path -Path .\upgrade_device.txt) 
+    {
+       start python azureClient.py 
+       #start ./go.bat
+        exit
+    }
+    Else
+    {
+        Start-Sleep -Seconds 3
+    }
+}
