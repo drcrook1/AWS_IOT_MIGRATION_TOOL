93849751bfb8dbbfa4af84298616e625d80143f0
johbake <johbake@microsoft.com>
2021-04-07 12:00:49 -0700

    launches aws and takes certs from local dir

--- Stream0/bootloader.ps1
+++ Stream0/bootloader.ps1
@@ -1,4 +1,7 @@
-﻿while ($TRUE)
+﻿$userprofiledir = $env.USERPROFILE
+Start-Process -FilePath python -ArgumentList "awsClient.py --thing-name JBThing1 --client-id JBThing1 --endpoint a1fxcu0l5seyuv-ats.iot.us-west-2.amazonaws.com --root-ca .\Amazon-root-CA-1.pem --cert .\device.pem.crt --key .\private.pem.key"
+
+while ($TRUE)
 {
     If (Test-Path -Path .\upgrade_device.txt) 
     {
@@ -6,7 +9,8 @@
        Start-Sleep -Seconds 2 #give python time to die before starting a new one
        
        $connectionstring = Get-Content -Path '.\deviceconnectionstring.txt'
-       start python azureClient.py --connectionstring $connectionstring
+       $args = "azureClient.py --connectionstring "+ $connectionstring
+       Start-Process -FilePath python -ArgumentList $args
        #start ./go.bat
         exit
     }
