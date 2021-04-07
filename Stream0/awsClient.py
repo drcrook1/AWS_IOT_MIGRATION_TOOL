0a2520a9f3faabfdbdef42f4155e6b57088c47b3
johbake <johbake@microsoft.com>
2021-04-06 21:46:17 -0700

    updated to run AWS jobs
    listens for jobs and, when one happens, creates a file the bootloader watches for

--- Stream0/awsClient.py
+++ Stream0/awsClient.py
@@ -3,31 +3,48 @@
 
 import argparse
 from random import randint 
-from awscrt import io, mqtt, auth, http
+from awscrt import auth, http, io, mqtt
+from awsiot import iotjobs
 from awsiot import mqtt_connection_builder
+from concurrent.futures import Future
 import sys
 import threading
 import time
+import traceback
 from uuid import uuid4
 
-# This sample uses the Message Broker for AWS IoT to send and receive messages
-# through an MQTT connection. On startup, the device connects to the server,
-# subscribes to a topic, and begins publishing messages to that topic.
-# The device should receive those same messages back from the message broker,
-# since it is subscribed to that same topic.
+# - Overview -
+# This sample uses the AWS IoT Jobs Service to receive and execute operations
+# on the device. Imagine periodic software updates that must be sent to and
+# executed on devices in the wild.
+#
+# - Instructions -
+# This sample requires you to create jobs for your device to execute. See:
+# https://docs.aws.amazon.com/iot/latest/developerguide/create-manage-jobs.html
+#
+# - Detail -
+# On startup, the sample tries to start the next pending job execution.
+# If such a job exists, the sample emulates "doing work" by spawning a thread
+# that sleeps for several seconds before marking the job as SUCCEEDED. When no
+# pending job executions exist, the sample sits in an idle state.
+#
+# The sample also subscribes to receive "Next Job Execution Changed" events.
+# If the sample is idle, this event wakes it to start the job. If the sample is
+# already working on a job, it remembers to try for another when it's done.
+# This event is sent by the service when the current job completes, so the
+# sample will be continually prompted to try another job until none remain.
 
-parser = argparse.ArgumentParser(description="Send and receive messages through and MQTT connection.")
+parser = argparse.ArgumentParser(description="Jobs sample runs all pending job executions.")
 parser.add_argument('--endpoint', required=True, help="Your AWS IoT custom endpoint, not including a port. " +
-                                                      "Ex: \"abcd123456wxyz-ats.iot.us-east-1.amazonaws.com\"")
-parser.add_argument('--cert', help="File path to your client certificate, in PEM format.")
-parser.add_argument('--key', help="File path to your private key, in PEM format.")
+                                                      "Ex: \"w6zbse3vjd5b4p-ats.iot.us-west-2.amazonaws.com\"")
+parser.add_argument('--cert', help="File path to your client certificate, in PEM format")
+parser.add_argument('--key', help="File path to your private key file, in PEM format")
 parser.add_argument('--root-ca', help="File path to root certificate authority, in PEM format. " +
                                       "Necessary if MQTT server uses a certificate that's not already in " +
-                                      "your trust store.")
+                                      "your trust store")
 parser.add_argument('--client-id', default="test-" + str(uuid4()), help="Client ID for MQTT connection.")
-parser.add_argument('--topic', default="test/topic", help="Topic to subscribe to, and publish messages to.")
-parser.add_argument('--count', default=10, type=int, help="Number of messages to publish/receive before exiting. " +
-                                                          "Specify 0 to run forever.")
+parser.add_argument('--thing-name', required=True, help="The name assigned to your IoT Thing")
+parser.add_argument('--job-time', default=5, type=float, help="Emulate working on job by sleeping this many seconds.")
 parser.add_argument('--use-websocket', default=False, action='store_true',
     help="To use a websocket instead of raw mqtt. If you " +
     "specify this option you must specify a region for signing, you can also enable proxy mode.")
@@ -40,49 +57,184 @@ parser.add_argument('--verbosity', choices=[x.name for x in io.LogLevel], defaul
     help='Logging level')
 
 # Using globals to simplify sample code
-args = parser.parse_args()
+is_sample_done = threading.Event()
 
-io.init_logging(getattr(io.LogLevel, args.verbosity), 'stderr')
+mqtt_connection = None
+jobs_client = None
+thing_name = ""
 
-received_count = 0
-received_all_event = threading.Event()
+class LockedData:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.disconnect_called = False
+        self.is_working_on_job = False
+        self.is_next_job_waiting = False
 
-# Callback when connection is accidentally lost.
-def on_connection_interrupted(connection, error, **kwargs):
-    print("Connection interrupted. error: {}".format(error))
+locked_data = LockedData()
 
+# Function for gracefully quitting this sample
+def exit(msg_or_exception):
+    if isinstance(msg_or_exception, Exception):
+        print("Exiting Sample due to exception.")
+        traceback.print_exception(msg_or_exception.__class__, msg_or_exception, sys.exc_info()[2])
+    else:
+        print("Exiting Sample:", msg_or_exception)
 
-# Callback when an interrupted connection is re-established.
-def on_connection_resumed(connection, return_code, session_present, **kwargs):
-    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+    with locked_data.lock:
+        if not locked_data.disconnect_called:
+            print("Disconnecting...")
+            locked_data.disconnect_called = True
+            future = mqtt_connection.disconnect()
+            future.add_done_callback(on_disconnected)
 
-    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
-        print("Session did not persist. Resubscribing to existing topics...")
-        resubscribe_future, _ = connection.resubscribe_existing_topics()
+def try_start_next_job():
+    print("Trying to start the next job...")
+    with locked_data.lock:
+        if locked_data.is_working_on_job:
+            print("Nevermind, already working on a job.")
+            return
 
-        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
-        # evaluate result with a callback instead.
-        resubscribe_future.add_done_callback(on_resubscribe_complete)
+        if locked_data.disconnect_called:
+            print("Nevermind, sample is disconnecting.")
+            return
 
+        locked_data.is_working_on_job = True
+        locked_data.is_next_job_waiting = False
 
-def on_resubscribe_complete(resubscribe_future):
-        resubscribe_results = resubscribe_future.result()
-        print("Resubscribe results: {}".format(resubscribe_results))
+    print("Publishing request to start next job...")
+    request = iotjobs.StartNextPendingJobExecutionRequest(thing_name=args.thing_name)
+    publish_future = jobs_client.publish_start_next_pending_job_execution(request, mqtt.QoS.AT_LEAST_ONCE)
+    publish_future.add_done_callback(on_publish_start_next_pending_job_execution)
 
-        for topic, qos in resubscribe_results['topics']:
-            if qos is None:
-                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+def done_working_on_job():
+    with locked_data.lock:
+        locked_data.is_working_on_job = False
+        try_again = locked_data.is_next_job_waiting
 
+    if try_again:
+        try_start_next_job()
 
-# Callback when the subscribed topic receives a message
-def on_message_received(topic, payload, dup, qos, retain, **kwargs):
-    print("Received message from topic '{}': {}".format(topic, payload))
-    global received_count
-    received_count += 1
-    if received_count == args.count:
-        received_all_event.set()
+def on_disconnected(disconnect_future):
+    # type: (Future) -> None
+    print("Disconnected.")
+
+    # Signal that sample is finished
+    is_sample_done.set()
+
+def on_next_job_execution_changed(event):
+    # type: (iotjobs.NextJobExecutionChangedEvent) -> None
+    try:
+        execution = event.execution
+        if execution:
+            print("Received Next Job Execution Changed event. job_id:{} job_document:{}".format(
+                execution.job_id, execution.job_document))
+
+            # Start job now, or remember to start it when current job is done
+            start_job_now = False
+            with locked_data.lock:
+                if locked_data.is_working_on_job:
+                    locked_data.is_next_job_waiting = True
+                else:
+                    start_job_now = True
+
+            if start_job_now:
+                try_start_next_job()
+
+        else:
+            print("Received Next Job Execution Changed event: None. Waiting for further jobs...")
+
+    except Exception as e:
+        exit(e)
+
+def on_publish_start_next_pending_job_execution(future):
+    # type: (Future) -> None
+    try:
+        future.result() # raises exception if publish failed
+
+        print("Published request to start the next job.")
+
+    except Exception as e:
+        exit(e)
+
+def on_start_next_pending_job_execution_accepted(response):
+    # type: (iotjobs.StartNextJobExecutionResponse) -> None
+    try:
+        if response.execution:
+            execution = response.execution
+            print("Request to start next job was accepted. job_id:{} job_document:{}".format(
+                execution.job_id, execution.job_document))
+
+            # To emulate working on a job, spawn a thread that sleeps for a few seconds
+            job_thread = threading.Thread(
+                target=lambda: job_thread_fn(execution.job_id, execution.job_document),
+                name='job_thread')
+            job_thread.start()
+        else:
+            print("Request to start next job was accepted, but there are no jobs to be done. Waiting for further jobs...")
+            done_working_on_job()
+
+    except Exception as e:
+        exit(e)
+
+def on_start_next_pending_job_execution_rejected(rejected):
+    # type: (iotjobs.RejectedError) -> None
+    exit("Request to start next pending job rejected with code:'{}' message:'{}'".format(
+        rejected.code, rejected.message))
+
+def job_thread_fn(job_id, job_document):
+    try:
+        print("Starting local work on job...")
+
+        print("Creating local file.")
+        f = open("upgradedevice.txt", "a")
+        f.write ("AWS job run to upgrade firmware")
+        f.close()
+
+        #time.sleep(args.job_time)
+        print("Done working on job.")
+
+        print("Publishing request to update job status to SUCCEEDED...")
+        request = iotjobs.UpdateJobExecutionRequest(
+            thing_name=args.thing_name,
+            job_id=job_id,
+            status=iotjobs.JobStatus.SUCCEEDED)
+        publish_future = jobs_client.publish_update_job_execution(request, mqtt.QoS.AT_LEAST_ONCE)
+        publish_future.add_done_callback(on_publish_update_job_execution)
+
+        print("Exiting this client....")
+        exit (1)
+
+    except Exception as e:
+        exit(e)
+
+def on_publish_update_job_execution(future):
+    # type: (Future) -> None
+    try:
+        future.result() # raises exception if publish failed
+        print("Published request to update job.")
+
+    except Exception as e:
+        exit(e)
+
+def on_update_job_execution_accepted(response):
+    # type: (iotjobs.UpdateJobExecutionResponse) -> None
+    try:
+        print("Request to update job was accepted.")
+        done_working_on_job()
+    except Exception as e:
+        exit(e)
+
+def on_update_job_execution_rejected(rejected):
+    # type: (iotjobs.RejectedError) -> None
+    exit("Request to update job status was rejected. code:'{}' message:'{}'.".format(
+        rejected.code, rejected.message))
 
 if __name__ == '__main__':
+    # Process input args
+    args = parser.parse_args()
+    thing_name = args.thing_name
+    io.init_logging(getattr(io.LogLevel, args.verbosity), 'stderr')
+
     # Spin up resources
     event_loop_group = io.EventLoopGroup(1)
     host_resolver = io.DefaultHostResolver(event_loop_group)
@@ -101,8 +253,6 @@ if __name__ == '__main__':
             credentials_provider=credentials_provider,
             websocket_proxy_options=proxy_options,
             ca_filepath=args.root_ca,
-            on_connection_interrupted=on_connection_interrupted,
-            on_connection_resumed=on_connection_resumed,
             client_id=args.client_id,
             clean_session=False,
             keep_alive_secs=6)
@@ -114,8 +264,6 @@ if __name__ == '__main__':
             pri_key_filepath=args.key,
             client_bootstrap=client_bootstrap,
             ca_filepath=args.root_ca,
-            on_connection_interrupted=on_connection_interrupted,
-            on_connection_resumed=on_connection_resumed,
             client_id=args.client_id,
             clean_session=False,
             keep_alive_secs=6)
@@ -123,53 +271,95 @@ if __name__ == '__main__':
     print("Connecting to {} with client ID '{}'...".format(
         args.endpoint, args.client_id))
 
-    connect_future = mqtt_connection.connect()
+    connected_future = mqtt_connection.connect()
 
-    # Future.result() waits until a result is available
-    connect_future.result()
+    jobs_client = iotjobs.IotJobsClient(mqtt_connection)
+
+    # Wait for connection to be fully established.
+    # Note that it's not necessary to wait, commands issued to the
+    # mqtt_connection before its fully connected will simply be queued.
+    # But this sample waits here so it's obvious when a connection
+    # fails or succeeds.
+    connected_future.result()
     print("Connected!")
 
-    # Subscribe
-    print("Subscribing to topic '{}'...".format(args.topic))
-    subscribe_future, packet_id = mqtt_connection.subscribe(
-        topic=args.topic,
-        qos=mqtt.QoS.AT_LEAST_ONCE,
-        callback=on_message_received)
+    try:
+        # Subscribe to necessary topics.
+        # Note that is **is** important to wait for "accepted/rejected" subscriptions
+        # to succeed before publishing the corresponding "request".
+        print("Subscribing to Next Changed events...")
+        changed_subscription_request = iotjobs.NextJobExecutionChangedSubscriptionRequest(
+            thing_name=args.thing_name)
 
-    subscribe_result = subscribe_future.result()
-    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+        subscribed_future, _ = jobs_client.subscribe_to_next_job_execution_changed_events(
+            request=changed_subscription_request,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+            callback=on_next_job_execution_changed)
 
-    # Publish message to server desired number of times.
-    # This step is skipped if message is blank.
-    # This step loops forever if count was set to 0.
-    if args.endpoint:
-        if args.count == 0:
-            print ("Sending messages until program killed")
-        else:
-            print ("Sending {} message(s)".format(args.count))
+        # Wait for subscription to succeed
+        subscribed_future.result()
+
+        print("Subscribing to Start responses...")
+        start_subscription_request = iotjobs.StartNextPendingJobExecutionSubscriptionRequest(
+            thing_name=args.thing_name)
+        subscribed_accepted_future, _ = jobs_client.subscribe_to_start_next_pending_job_execution_accepted(
+            request=start_subscription_request,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+            callback=on_start_next_pending_job_execution_accepted)
+
+        subscribed_rejected_future, _ = jobs_client.subscribe_to_start_next_pending_job_execution_rejected(
+            request=start_subscription_request,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+            callback=on_start_next_pending_job_execution_rejected)
+
+        # Wait for subscriptions to succeed
+        subscribed_accepted_future.result()
+        subscribed_rejected_future.result()
+
+        print("Subscribing to Update responses...")
+        # Note that we subscribe to "+", the MQTT wildcard, to receive
+        # responses about any job-ID.
+        update_subscription_request = iotjobs.UpdateJobExecutionSubscriptionRequest(
+                thing_name=args.thing_name,
+                job_id='+')
+
+        subscribed_accepted_future, _ = jobs_client.subscribe_to_update_job_execution_accepted(
+            request=update_subscription_request,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+            callback=on_update_job_execution_accepted)
+
+        subscribed_rejected_future, _ = jobs_client.subscribe_to_update_job_execution_rejected(
+            request=update_subscription_request,
+            qos=mqtt.QoS.AT_LEAST_ONCE,
+            callback=on_update_job_execution_rejected)
+
+        # Wait for subscriptions to succeed
+        subscribed_accepted_future.result()
+        subscribed_rejected_future.result()
+
+        # Make initial attempt to start next job. The service should reply with
+        # an "accepted" response, even if no jobs are pending. The response
+        # will contain data about the next job, if there is one.
+        try_start_next_job()
+
+        print ("Sending message(s)")
 
         publish_count = 1
-        while (publish_count <= args.count) or (args.count == 0):
+        while (publish_count <= 100) :
             temperature = 80-publish_count
             message = "{\"devID\":\""+ args.client_id +"\",\"temperature\":" + str(randint(50,100)) + ",\"humidity\":" + str(randint(10,99))+"}"
-            print("Publishing message to topic '{}': {}".format(args.topic, message))
+            print("Publishing message to topic '{}': {}".format("test/topic", message))
             mqtt_connection.publish(
-                topic=args.topic,
+                topic="test/topic",
                 payload=message,
                 qos=mqtt.QoS.AT_LEAST_ONCE)
-            time.sleep(1)
+            time.sleep(10)
             publish_count += 1
 
-    # Wait for all messages to be received.
-    # This waits forever if count was set to 0.
-    if args.count != 0 and not received_all_event.is_set():
-        print("Waiting for all messages to be received...")
 
-    received_all_event.wait()
-    print("{} message(s) received.".format(received_count))
+    except Exception as e:
+        exit(e)
 
-    # Disconnect
-    print("Disconnecting...")
-    disconnect_future = mqtt_connection.disconnect()
-    disconnect_future.result()
-    print("Disconnected!")
+
+    # Wait for the sample to finish
+    is_sample_done.wait()
