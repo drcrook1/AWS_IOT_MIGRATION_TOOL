0b52115c5190db1b15c2bfd8b8d685a99a2fbd36
johbake <johbake@microsoft.com>
2021-04-08 10:36:56 -0700

    converted to take json input

--- Stream0/createawsupgradejob.py
+++ Stream0/createawsupgradejob.py
@@ -5,14 +5,14 @@ from uuid import uuid4
 import os
 
 parser = argparse.ArgumentParser(description="Create an AWS job to migrate an IoT Thing to Azure")
-parser.add_argument('--thing-name', required=True, help="Thing name. ")
-parser.add_argument('--thing-arn', required=True, help="Thing arn. ")
 parser.add_argument('--s3bucket', required=True, help="Bucket with azure code. ")
 parser.add_argument('--s3bucket-key-id', required=True, help="Bucket key ID. ")
 parser.add_argument('--s3bucket-key', required=True, help="Bucket key. ")
+parser.add_argument('--jsontoprocess', required=True, help="aws-things-azure-processed file")
 args = parser.parse_args()
 
 if __name__ == '__main__':
+
     client = boto3.client(
         'iot',
         region_name=os.environ["AWS_REGION"],
@@ -20,30 +20,38 @@ if __name__ == '__main__':
         aws_secret_access_key=os.environ["AWS_KEY"]
     )
 
-    response = client.create_job(
-        jobId='upgrade-'+args.thing_name + "-" + str(uuid4()),
-        targets=[
-            args.thing_arn,
-        ],
-        document="{ \"operation\": \"upgradetoAzure\", \"fileBucket\": \""+args.s3bucket+"\", \"ACCESS_KEY\": \""+args.s3bucket_key_id + "\",\"SECRET_KEY\": \""+args.s3bucket_key + "\", \"AZURE_CONNECTION_STRING\": \""+"HostName=johnbakairlift.azure-devices.net;DeviceId=JBThing1;SharedAccessKey=jgQ94w6mjFWioNNBj1TG4aKx4pCZypwvyy4sFstuHjk=" + "\" }",
-        jobExecutionsRolloutConfig={
-            'maximumPerMinute': 5,
-            'exponentialRate': {
-                'baseRatePerMinute': 5,
-                'incrementFactor': 1.1,
-                'rateIncreaseCriteria': {
-                    'numberOfNotifiedThings': 1
+    with open (args.jsontoprocess) as file:
+        jsonJobDoc = json.load(file)
+    
+    for thing in jsonJobDoc['things']:
+        print (thing['thingName'])
+        print (thing['thingArn'])
+        print (thing['azure']['iotconnstr'])
+
+        response = client.create_job(
+            jobId='upgrade-'+thing['thingName'] + "-" + str(uuid4()),
+            targets=[
+                thing['thingArn'],
+            ],
+            document="{ \"operation\": \"upgradetoAzure\", \"fileBucket\": \""+args.s3bucket+"\", \"ACCESS_KEY\": \""+args.s3bucket_key_id + "\",\"SECRET_KEY\": \""+args.s3bucket_key + "\", \"AZURE_CONNECTION_STRING\": \""+thing['azure']['iotconnstr'] + "\" }",
+            jobExecutionsRolloutConfig={
+                'maximumPerMinute': 5,
+                'exponentialRate': {
+                    'baseRatePerMinute': 5,
+                    'incrementFactor': 1.1,
+                    'rateIncreaseCriteria': {
+                        'numberOfNotifiedThings': 1
+                    }
                 }
-            }
-        },
-        abortConfig={
-            'criteriaList': [
-                {
-                    'failureType': 'FAILED',
-                    'action': 'CANCEL',
-                    'thresholdPercentage': 100,
-                    'minNumberOfExecutedThings': 1
-                },
-            ]
-        }    
-    )
+            },
+            abortConfig={
+                'criteriaList': [
+                    {
+                        'failureType': 'FAILED',
+                        'action': 'CANCEL',
+                        'thresholdPercentage': 100,
+                        'minNumberOfExecutedThings': 1
+                    },
+                ]
+            }    
+        )
