acf9ebf9e78c123b34065ef5181efc31368e3de2
johbake <johbake@microsoft.com>
2021-04-09 11:22:59 -0700

    changed start-process to work in containers

--- DEVICE_SIMULATOR/bootloader.ps1
+++ DEVICE_SIMULATOR/bootloader.ps1
@@ -1,18 +1,22 @@
 ﻿$userprofiledir = $env.USERPROFILE
-Start-Process -FilePath python -ArgumentList "awsClient.py --thing-name JBThing1 --client-id JBThing1 --endpoint a1fxcu0l5seyuv-ats.iot.us-west-2.amazonaws.com --root-ca .\Amazon-root-CA-1.pem --cert .\device.pem.crt --key .\private.pem.key"
+Start-Process -FilePath python3 -ArgumentList "awsClient.py --thing-name JBThing1 --client-id JBThing1 --endpoint a1fxcu0l5seyuv-ats.iot.us-west-2.amazonaws.com --root-ca Amazon-root-CA-1.pem --cert device.pem.crt --key private.pem.key"
 
 while ($TRUE)
 {
     If (Test-Path -Path .\upgrade_device.txt) 
     {
-       Stop-Process -Name python -Force -ErrorAction SilentlyContinue
+       Stop-Process -Name python3 -Force -ErrorAction SilentlyContinue
        Start-Sleep -Seconds 2 #give python time to die before starting a new one
        
        $connectionstring = Get-Content -Path '.\deviceconnectionstring.txt'
        $args = "azureClient.py --connectionstring "+ $connectionstring
-       Start-Process -FilePath python -ArgumentList $args
+       Start-Process -FilePath python3 -ArgumentList $args
        #start ./go.bat
-       exit
+       while ($TRUE)
+       {
+        Start-Sleep -Seconds 60
+       }
+       
     }
     Else
     {
